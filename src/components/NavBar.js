86ebf694651feaadf0d3770bf1fe04f4bcfcbf5c
LucasGut05 <70763377+LucasGut05@users.noreply.github.com>
2022-07-14 00:49:39 -0300

    Subida desafio 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <nav className="NavBar">
+      <h1>Lucas Gutierrez</h1>
+      <ul className="ul">
+        <li>Acerca de</li>
+        <li>Productos</li>
+        <li>Promociones</li>
+      </ul>
+    </nav>
+  );
+};
